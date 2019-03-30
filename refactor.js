2e2ec314c4a63c155e6d4be6cd16254654887c83
Luci Peterson <lucijpeterson@gmail.com>
2019-03-30 15:32:25 -0700

    Code clean up
    Consolidated functions into one line each.  Removed parentheses on the single parameter on multiplyByTen.  Added semi-colons at the end of lines.

--- refactor.js
+++ refactor.js
@@ -1,13 +1,9 @@
-const multiplyByTen = (num) => {
-	return 10 * num;
-}
+const multiplyByTen = num => {return 10 * num;};
 
-const printToTen = () => {
-	for (let i = 1; i <= 10; i++) {
-		console.log(i);
-	}
-}
+const printToTen = () => {for (let i = 1; i <= 10; i++) {console.log(i);}};
 
-const addUs = (num1, num2) => {
-	console.log(num1 + num2);
-}
+const addUs = (num1, num2) => {console.log(num1 + num2);};
+
+console.log(multiplyByTen(2));
+printToTen();
+addUs(3,5);
